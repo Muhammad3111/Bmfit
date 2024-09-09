b1d2b572a11e21dfafb8461aec181ddc9b0d8b0b
Muhammad3111 <karimovmuhammad3502@gmail.com>
2024-09-09 16:19:22 +0500

    fixed logo image

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import Banner from "./components/banner/Banner";
 import Hero1 from "./components/hero-1/Hero";
 import Hero2 from "./components/hero-2/Hero";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
     return (
